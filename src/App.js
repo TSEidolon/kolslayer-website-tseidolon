764520761e80f107aae182146f19be7be22febd7
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-06-27 08:54:07 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/navBar.jsx"
+
+
+function App() {
+  return (
+    <div>
+      <p className='text-9xl'> Hello </p>
+      <Navbar />
+    </div>
+
+  );
+}
+
+export default App;
