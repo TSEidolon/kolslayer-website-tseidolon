53c2353ecb74b8bd140587c5dac0ed33b8453fc7
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-07-09 17:43:29 -0600

    edited game section and added assets

--- src/components/gameInfo.jsx
+++ src/components/gameInfo.jsx
@@ -1,16 +1,18 @@
-
+import pineTree from '../assets/pine-tree.png'
 
 export function GameInfo () {
   return(
-    <div className="h-screen flex justify-center items-start flex-col">
+    <div className="h-screen flex justify-center items-start flex-col overflow-x-hidden">
       <h1 className="border-2 py-5 px-[100px] text-2xl"> Game Info </h1>
       <div className="h-full flex w-full">
         <section className="flex justify-center items-center gap-10 border-2 w-[60%] pl-10 h-[90%]">
           <div className="border-2  text-xl flex justify-center items-center flex-col min-w-[20%] h-full gap-10">
-            <p> Title 1 </p>
-            <p> Title 2 </p>
-            <p> Title 3 </p>
-            <p> Title 4 </p>
+            <p className=" px-2 border-2 border-black w-full text-center
+            relative  transition-all ease-in-out  after:transition-[width] after:ease-in-out after:duration-500 after:absolute after:bg-black after:origin-center after:h-full after:w-0 hover:after:w-[12%] after:right-[0%]"> Title 1 </p>
+
+            <p className="border-2 border-black  w-full text-center"> Title 2 </p>
+            <p className="border-2 border-black  w-full text-center"> Title 3 </p>
+            <p className="border-2 border-black  w-full text-center"> Title 4 </p>
           </div>
           <div className="flex justify-center items-center flex-col gap-10">
             <h2 className="text-2xl"> Title of Paragraph </h2>
@@ -19,7 +21,8 @@ export function GameInfo () {
         </section>
         <section className="pl-10  pb-[100px] flex justify-center items-center w-[40%]">
           <div className="bg-game-pattern-one relative h-[400px] w-[400px]  bg-cover bg-no-repeat border-2 rotate-45">
-
+            <img src={pineTree} alt="Pine Tree" className='rotate-[-45deg]' />
+            
           </div>
         </section>
       </div>
