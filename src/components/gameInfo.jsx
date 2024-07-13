b930c322fa47ad6088e5389b445272731fd1c6f1
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-07-12 22:13:54 -0600

    edited game info

--- src/components/gameInfo.jsx
+++ src/components/gameInfo.jsx
@@ -12,7 +12,10 @@ export function GameInfo () {
 
   return(
     <div className="h-screen flex justify-center items-start flex-col overflow-x-hidden text-white bg-[#3183C5] bg-game-info-background bg-left bg-no-repeat bg-[length:75%]">
-      <h1 className="border-2 py-5 px-[100px] text-2xl"> Game Info </h1>
+      <div className='relative'>
+      <h1 className="border-2 py-5 px-[100px] text-2xl game-info-top"> Game Info </h1>
+      </div>
+      
       <div className="h-full flex w-full">
         <section className="flex justify-center items-center gap-10 border-2 w-[60%] pl-10 h-[90%]">
           <div className="border-2  text-xl flex justify-center items-center flex-col min-w-[20%] h-full gap-10">
@@ -59,7 +62,7 @@ export function GameInfo () {
             <p className="text-lg"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias tempora repellat odit possimus fugit modi doloremque, facere commodi illo quod quia quibusdam quos dolore sit qui animi suscipit impedit quis rem. Dolor deserunt velit similique aliquid. Vero a nobis voluptatum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, natus.</p>
           </div>
         </section>
-        <section className="pl-10  pb-[100px] flex justify-center items-center w-[40%] select-none">
+        <section className="pl-10  pb-[110px] flex justify-center items-center w-[40%] select-none">
           <div className="bg-game-pattern-one relative h-[400px] w-[400px]  bg-cover bg-no-repeat border-2 rotate-45">
             <img src={pineTree} alt="Pine Tree" className='rotate-[-45deg]' />
             
