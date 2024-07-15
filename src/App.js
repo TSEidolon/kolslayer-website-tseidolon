c0f21abeb2bbd90481a68bdc002d49e14e089b11
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-07-15 17:11:37 -0600

    played around with parallax on the parallax test component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <NavBarTest />
       <GameInfo />
       <TestComponent />
+
     </div>
 
   );
--- src/components/parallaxTest.jsx
+++ src/components/parallaxTest.jsx
@@ -1,12 +1,19 @@
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { GameInfo } from './gameInfo'
 // https://www.youtube.com/watch?v=jeSPvDI-IJw&list=PLkY85cDHOEpuxnzOBj2Cl5UEIFfKVBQq7&index=3
 
 export function TestComponent() {
   return (
-    <Parallax pages={1} style={{ }}>
-      <ParallaxLayer offset={0} speed={2.5}>
-        <p>Parallax</p>
-      </ParallaxLayer>
+    <Parallax pages={2} style={{ }} class="animation parallax-remove-scrollbar">
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div class="animation_layer parallax" id="background"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.4}>
+          <div class="animation_layer parallax" id="foreground"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.25}>
+          <GameInfo />
+        </ParallaxLayer>
     </Parallax>
   )
 }
