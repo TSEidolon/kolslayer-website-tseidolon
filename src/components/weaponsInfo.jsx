5e68697780c6a112faa6e76176346dfb5bf60d17
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-07-20 06:47:56 -0600

    started the plan for the weapons info section

--- src/components/weaponsInfo.jsx
+++ src/components/weaponsInfo.jsx
@@ -1,8 +1,12 @@
 import React from "react"
 
 
-// const todo = {["cardsDesign"],["background"],["useState for cards"]}
+// const todo = {["cardsDesign"],["background"],["fix useState for cards"]}
 export function WeaponsInfo () {
+  // const [weaponsInfoOpen, setweaponsInfoOpen] = React.useState(1);
+  // const handleTitle = (e) => {
+  //   setweaponsInfoOpen(e)
+  // }
   return (
     <div className="h-screen flex justify-center items-start flex-col overflow-x-hidden overflow-y-hidden text-white bg-[#3183C5]">
       <div className='relative'>
