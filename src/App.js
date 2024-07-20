54086ce1e149e31f03af57c3839b328c96a057bd
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-07-20 06:42:49 -0600

    started the weapons info section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { GameInfo } from "./components/gameInfo.jsx";
 // import { GameInfoTest } from "./components/gameInfoTest.jsx";
 // import { TestComponent } from "./components/parallaxTest.jsx";
 import { ParallaxProvider } from "react-scroll-parallax";
+import { WeaponsInfo } from "./components/weaponsInfo.jsx";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <ParallaxProvider >
         <NavBarTest />
         <GameInfo />
+        <WeaponsInfo />
       </ParallaxProvider> 
       
     </div>
---
+++ src/components/weaponsInfo.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+// const todo = {["cardsDesign"],["background"],["useState for cards"]}
+export function WeaponsInfo () {
+  return (
+    <div className="h-screen flex justify-center items-start flex-col overflow-x-hidden overflow-y-hidden text-white bg-[#3183C5]">
+      <div className='relative'>
+        <h1 className=" py-5 px-[100px] text-2xl weapons-info-top"> Weapons Info </h1>
+      </div>
+      <section>
+        <p>card 1</p>
+        <p>card 2</p>
+        <p>card 3</p>
+      </section>
+    </div>
+    
+
+  )
+}
