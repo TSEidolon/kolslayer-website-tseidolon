db246b591c56244fd87d20f05a824589d803bcc1
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-07-29 22:43:00 -0600

    edited weapons section

--- src/components/weaponsInfo.jsx
+++ src/components/weaponsInfo.jsx
@@ -9,12 +9,12 @@ export function WeaponsInfo () {
   //   setweaponsInfoOpen(e)
   // }
   return (
-    <div className="h-screen flex justify-center items-start flex-col overflow-x-hidden overflow-y-hidden text-white bg-[#3183C5]">
+    <div className="h-screen flex items-start flex-col overflow-x-hidden overflow-y-hidden text-white bg-[#3183C5]">
       <div className='relative'>
         <h1 className=" py-5 px-[100px] text-2xl weapons-info-top"> Weapons Info </h1>
       </div>
-      <section className="w-full">
-        <div className="h-[100%] w-full flex justify-center items-stretch gap-[10rem] ">
+      <section className="w-full h-full ">
+        <div className="h-[100%] w-full flex justify-center items-center gap-[10rem] ">
           <div className=" h-[440px] w-[290px] border-white border-2 flex justify-center items-center flex-col rounded-xl bg-black bg-opacity-20 pt-2">
             <div className="">
               <h2 className="text-3xl pb-2"> Sword </h2>
@@ -42,8 +42,6 @@ export function WeaponsInfo () {
               <p className=" text-lg 2 text-center h-full flex items-center pr-5"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet..</p>
             </div>
           </div>
-
-
         </div>
 
       </section>
