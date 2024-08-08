7981ae4fb4fc15345eb134b638499bb902f6c6eb
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-08-07 23:54:58 -0600

    added weaponsInfoTest test code into weaponsInfo

--- src/components/weaponsInfo.jsx
+++ src/components/weaponsInfo.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import sword from "../assets/sword-id-6331871.png"
+import axe from "../assets/axe.png"
+import mace from "../assets/mace.png"
 
 import { Parallax } from "react-scroll-parallax"
 
@@ -7,11 +9,14 @@ import { Parallax } from "react-scroll-parallax"
 
 // const todo = {["background"],["fix useState for cards"]}
 export function WeaponsInfo () {
-  // const [weaponsInfoOpen, setweaponsInfoOpen] = React.useState(1);
-  // const handleTitle = (e) => {
-  //   setweaponsInfoOpen(e)
-  // }
-  
+  const [weaponsInfoOpen, setweaponsInfoOpen] = React.useState(1);
+  const handleCards = (e) => {
+    setweaponsInfoOpen(e)
+  }
+  function checkClick() {
+    console.log (weaponsInfoOpen)
+  }
+
 
  
   return (
@@ -22,32 +27,36 @@ export function WeaponsInfo () {
         
       </div>
       <Parallax speed={10}  className="w-full h-full">
-      <section className="w-full h-full bg-sword-background bg-contain bg-center  bg-no-repeat ">
+      <section className={"w-full h-full  bg-contain bg-no-repeat  " + (
+        weaponsInfoOpen===1 ? "bg-sword-background bg-left": 
+        weaponsInfoOpen===2 ? "bg-axe-background bg-center " :  
+        "bg-mace-background bg-right"
+      )}>
         <div className=" w-full h-full flex justify-center items-center gap-[10rem] z-20 relative">
-          <div className=" h-[440px] w-[290px] border-white border-2 flex justify-center items-center flex-col rounded-xl bg-black bg-opacity-30 pt-2">
+          <div className=" h-[440px] w-[290px] border-white border-2 flex justify-center items-center flex-col rounded-xl bg-black bg-opacity-30 pt-2" onClick={() => {handleCards(1);checkClick()}}>
             <div className="">
               <h2 className="text-3xl pb-2"> Sword </h2>
             </div>
             <div className="flex justify-stretch items-center  h-[70%] ">
-              <img src={sword} alt="sword-image" className="w-[40%] h-[225px]" />
+              <img src={sword} alt="sword-image" className="w-[35%] h-[225px] object-contain" />
               <p className=" text-lg 2 text-center h-full flex items-center pr-5"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet..</p>
             </div>
           </div>
-          <div className=" h-[440px] w-[290px] border-white border-2 flex justify-center items-center flex-col rounded-xl bg-black bg-opacity-30 pt-2">
+          <div className=" h-[440px] w-[290px] border-white border-2 flex justify-center items-center flex-col rounded-xl bg-black bg-opacity-30 pt-2" onClick={() => {handleCards(2);checkClick()}}>
             <div className="">
               <h2 className="text-3xl pb-2"> Sword </h2>
             </div>
-            <div className="flex justify-stretch items-center  h-[70%] ">
-              <img src={sword} alt="sword-image" className="w-[40%] h-[225px]" />
+            <div className="flex justify-stretch items-center pl-[10px] h-[70%] ">
+              <img src={axe} alt="axe-image" className="w-[40%] h-[300px] object-contain " />
               <p className=" text-lg 2 text-center h-full flex items-center pr-5"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet..</p>
             </div>
           </div>
-          <div className=" h-[440px] w-[290px] border-white border-2 flex justify-center items-center flex-col rounded-xl bg-black bg-opacity-30 pt-2">
+          <div className=" h-[440px] w-[290px] border-white border-2 flex justify-center items-center flex-col rounded-xl bg-black bg-opacity-30 pt-2" onClick={() => {handleCards(3);checkClick()}}>
             <div className="">
               <h2 className="text-3xl pb-2"> Sword </h2>
             </div>
             <div className="flex justify-stretch items-center  h-[70%] ">
-              <img src={sword} alt="sword-image" className="w-[40%] h-[225px]" />
+              <img src={mace} alt="sword-image" className="w-[35%] h-[225px] object-contain" />
               <p className=" text-lg 2 text-center h-full flex items-center pr-5"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet..</p>
             </div>
           </div>
