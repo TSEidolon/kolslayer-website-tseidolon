7f05cb2744fe8ba4615b4a798cb9263533c6ca30
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-08-13 08:34:38 -0600

    added assets and edited mediainfo section

--- src/components/mediaInfo.jsx
+++ src/components/mediaInfo.jsx
@@ -6,16 +6,17 @@ import image4 from "../assets/heather-shevlin-3B_NrzTjajc-unsplash.jpg"
 // import image2 from "../assets/giorgio-trovato-n4cjanwvGdg-unsplash.jpg"
 // import image2 from "../assets/giorgio-trovato-n4cjanwvGdg-unsplash.jpg"
 // import image2 from "../assets/giorgio-trovato-n4cjanwvGdg-unsplash.jpg"
+import backgroundImage1 from "../assets/WeirdShapeAbbomination.png"
 
-// 
+import { Parallax } from "react-scroll-parallax"
 
 export function MediaInfo (){
   return (
-    <div className="h-screen flex items-start flex-col overflow-x-hidden overflow-y-hidden  text-white bg-[#3183C5]">
+    <div className="h-screen flex items-start flex-col overflow-x-hidden overflow-y-hidden   text-white bg-[#3183C5]">
       <div className='relative'>
-        <h1 className=" py-5 px-[100px] text-2xl weapons-info-top"> Weapons Info </h1>
+        <h1 className=" py-5 px-[100px] text-2xl weapons-info-top"> Media Info </h1>
       </div>
-    <div className="h-full w-full flex justify-center items-start">
+    <div className="h-full w-full flex justify-center items-start z-20 relative">
       <section className="w-full max-w-6xl p-5 pb-10 mx-auto mb-10 gap-3 columns-3 space-y-3">
         <img src={image1} alt="" className="rounded"/>
         <img src={image2} alt="" className="rounded"/>
@@ -25,6 +26,10 @@ export function MediaInfo (){
         <img src={image3} alt="" className="rounded"/>
         <img src={image4} alt="" className="rounded"/>
       </section>
+      {/* <img src={backgroundImage1} alt="" className="absolute -z-10 " /> */}
+    <Parallax  speed={10} className="-z-10  absolute max-w-6xl">
+      <img src={backgroundImage1} alt="" className="  top-[-10%] right-[50%] h-[800px]" />
+    </Parallax>
     </div>
     </div>
   )
