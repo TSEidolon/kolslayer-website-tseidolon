222fca4626ef9a49073e925ffcd595fb6302316d
Edgar Francis Caballero <efrcaballero@gmail.com>
2024-08-16 07:26:25 -0600

    edited signininfo

--- src/components/signUpInfo.jsx
+++ src/components/signUpInfo.jsx
@@ -1,11 +1,16 @@
 import React from "react"
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaDiscord } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 // import { Parallax } from "react-scroll-parallax"
 
 export function SignUp () {
   return (
-    <div className="h-[70vh] flex flex-col items-center justify-center text-white bg-[#3183C5]">
-      <section>
-        <h1> Learn More </h1>
+    <div className="h-[70vh] flex flex-col items-center justify-center  text-white bg-[#3183C5]">
+      <section className="flex justify-center items-center flex-col gap-4">
+        <h1 className="text-4xl"> Learn More </h1>
           <form className="w-[400px] mx-auto">
             <div className="relative">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none h-[50px]">
@@ -14,19 +19,83 @@ export function SignUp () {
                   <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
                 </svg>
               </div>
-              <input type="text" className="bg-[#315A83] border border-[#315A83]  text-lg rounded-lg focus:outline-none focus:border-white focus:ring-sky-500 block w-full  ps-10 p-2.5 " placeholder="Enter Your Email"/>
+              <input type="text" className="bg-[#315A83] border border-[#315A83]  text-lg rounded-md focus:outline-none focus:border-white focus:ring-sky-500 block w-full  ps-10 p-2.5 " placeholder="Enter Your Email"/>
 
-              <button type="submit" className="text-xl border-2 border-white p-[5px]"> Newsletter Sign Up </button>
+              <button type="submit" className="text-xl  p-[10px] w-full "> Newsletter Sign Up </button>
             </div>
           </form>
-        <button> Button area </button>
       </section>
-      <section>
+      <section className="flex justify-center items-center gap-6 py-[20px]">
         <div>
-          <p>Icons</p>
+        <div className="flex justify-center align items-center gap-9 text-white
+          after:block
+          relative
+          after:content-[''] 
+          after:absolute 
+          after:translate-y-2/4 
+          after:left-[calc(100%+8px)] 
+          after:w-3 
+          after:h-px 
+          after:top-[-54%]
+          lg:flex-row
+          
+        ">
+          <span className="before:block before:absolute before:-inset-1 before:-rotate-45 before:border before:border-white relative inline-block 
+          hover:before:border-2
+          after:content-['\_\_\_'] 
+          after:absolute 
+          after:translate-y-2/4 
+          after:left-[calc(100%+8px)] 
+          after:w-3 
+          after:h-px 
+          after:top-[-54%]
+           ">
+
+            <FaInstagram className="text-xl relative"/>
+          </span>
+          <span className="before:block before:absolute before:-inset-1 before:-rotate-45 before:border before:border-white relative inline-block
+          hover:before:border-2
+          after:content-['\_\_\_'] 
+          after:absolute 
+          after:translate-y-2/4 
+          after:left-[calc(100%+8px)] 
+          after:w-3 
+          after:h-px 
+          after:top-[-54%]">
+            <FaFacebook className="text-xl relative"/>
+          </span>
+          <span className="before:block before:absolute before:-inset-1 before:-rotate-45 before:border before:border-white relative inline-block
+          hover:before:border-2
+          after:content-['\_\_\_'] 
+          after:absolute 
+          after:translate-y-2/4 
+          after:left-[calc(100%+8px)] 
+          after:w-3 
+          after:h-px 
+          after:top-[-54%]">
+            <FaXTwitter className="text-xl relative"/>
+          </span>
+          <span className="before:block before:absolute before:-inset-1 before:-rotate-45 before:border before:border-white relative inline-block
+          hover:before:border-2
+          after:content-['\_\_\_'] 
+          after:absolute 
+          after:translate-y-2/4 
+          after:left-[calc(100%+8px)] 
+          after:w-3 
+          after:h-px 
+          after:top-[-54%]">
+            <FaDiscord className="text-xl relative"/>
+          </span>
+          <span className="before:block before:absolute before:-inset-1 before:-rotate-45 before:border before:border-white relative inline-block
+          hover:before:border-2">
+            <FaYoutube className="text-xl relative"/>
+          </span>
+ 
+
+        </div>
         </div>
         <div>
-          <button>Buy Now</button>
+          <button className="text-3xl p-5 border-2">Buy Now</button>
         </div>
       </section>
     </div>
